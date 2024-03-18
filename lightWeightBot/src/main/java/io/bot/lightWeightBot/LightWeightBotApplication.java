2404523e8f6beb13b697b4a666167bb56cfc8819
TimBezh <144917452+TimBezh@users.noreply.github.com>
2024-03-18 22:27:44 +0300

    Update LightWeightBotApplication.java

--- lightWeightBot/src/main/java/io/bot/lightWeightBot/LightWeightBotApplication.java
+++ lightWeightBot/src/main/java/io/bot/lightWeightBot/LightWeightBotApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LightWeightBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LightWeightBotApplication.class, args);
-		int a;
 	}
 }
